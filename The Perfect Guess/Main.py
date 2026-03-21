4b0d95d5962a1f43fab6550f5f9692e0e0e704cb
Srabon D Costa <sdcmario1971@gmail.com>
2026-03-21 10:00:38 +0600

    Update Main.py

--- The Perfect Guess/Main.py
+++ The Perfect Guess/Main.py
@@ -1,8 +1,10 @@
 #THIS GAME INCLUDE MULTIPLE PLAYERS.
 
 import random
-def playGames ():
-    #Level selection :
+
+def playGames():
+
+    # Level selection
     print("Select the mode difficulty :")
     print("1.Easy(1-50)")
     print("2.Medium(1-100)")
@@ -17,80 +19,76 @@ def playGames ():
     elif choice == 3:
         upper = 500
     else:
-        print("Wrong Choice yoo!")
+        print("Wrong Choice!")
         return
 
-    
-    name1 = input("\nName of player 1 :")
-    name2 = input("Name of player 2 :")
+    name1 = input("\nName of player 1 : ")
+    name2 = input("Name of player 2 : ")
 
-    
-    #FOR PLAYER 1
+    # PLAYER 1
     print(f"\nPlayer {name1} is playing Now ---\n")
-    n1 = random.randint(1,upper)
+
+    n1 = random.randint(1, upper)
     a = -1
     guesses1 = 0
-    while(a != n1):
+
+    while a != n1:
         try:
             if guesses1 == 0:
-                a = int(input(f"Hey {name1}, please guess a number between 1 to {upper}: "))
+                a = int(input(f"Hey {name1}, guess a number between 1 and {upper}: "))
             else:
-                a = int(input(f"Please guess another number : "))
+                a = int(input("Guess again: "))
 
-            guesses1 +=1
+            guesses1 += 1
 
-            if ( a > n1):
-                print("<<Lower number please.>>")
+            if a > n1:
+                print("<<Lower number please>>")
+            elif a < n1:
+                print("<<Higher number please>>")
 
-            elif(a < n1) :
-                print("<<Higher number please.>>")
-    
-        except ValueError :
-            print(f"Hey,{name1} you entered the wrong value!")
+        except ValueError:
+            print(f"Hey {name1}, wrong input!")
             continue
-    
 
+    print(f"{name1}, you guessed the number in {guesses1} attempts.")
 
-    print(f"{name1},you made the guess in {guesses1 } attempts.")
-
-
-    
-    #FOR PLAYER 2
+    # PLAYER 2
     print(f"\nPlayer {name2} is playing Now ---\n")
-    n2 = random.randint(1,upper)
+
+    n2 = random.randint(1, upper)
     a = -1
     guesses2 = 0
-    while(a != n2):
+
+    while a != n2:
         try:
-            if guesses1 == 0:
-                a = int(input(f"Hey {name2}, please guess a number between 1 to {upper}: "))
+            if guesses2 == 0:
+                a = int(input(f"Hey {name2}, guess a number between 1 and {upper}: "))
             else:
-                a = int(input(f"Please guess another number : "))
+                a = int(input("Guess again: "))
 
-            guesses1 +=1
+            guesses2 += 1
 
-            if ( a > n2):
-                print("<<Lower number please.>>")
+            if a > n2:
+                print("<<Lower number please>>")
+            elif a < n2:
+                print("<<Higher number please>>")
 
-            elif(a < n2) :
-                print("<<Higher number please.>>")
-    
-        except ValueError :
-            print(f"Hey,{name2} you entered the wrong value!")
+        except ValueError:
+            print(f"Hey {name2}, wrong input!")
             continue
-    
-    print(f"{name2},you made the guess in {guesses2 } attempts.")
-    
+
+    print(f"{name2}, you guessed the number in {guesses2} attempts.")
+
+    # SCOREBOARD
     print("\nSCOREBOARD ---")
-    print(f"{name1} make the guess of number {n1} in {guesses1 } attempts.")
-    print(f"{name2} make the guess of number {n2} in {guesses2 } attempts.\n")
+    print(f"{name1} guessed number {n1} in {guesses1} attempts.")
+    print(f"{name2} guessed number {n2} in {guesses2} attempts.\n")
 
-
-    if(guesses1 > guesses2):
-        print(f"Congratulation {name2}, you win!")
-    elif(guesses1 == guesses2):
-        print("Match draw!")
+    if guesses1 > guesses2:
+        print(f"Congratulations {name2}, you win!")
+    elif guesses1 == guesses2:
+        print("Match Draw!")
     else:
-        print(f"Congratulation {name1}, you win!")
+        print(f"Congratulations {name1}, you win!")
 
 playGames()
